7478093ef33297e0e39b7b006d1f40e1c375f74b
tdson10a1 <tdson10a1@gmail.com>
2026-02-10 20:24:26 +0700

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,14 @@
+function upDate(previewPic) {
+    console.log(previewPic.alt);
+    console.log(previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
+}
+
+function undo() {
+    document.getElementById("image").innerHTML =
+        "Hover over an image below to display here.";
+    document.getElementById("image").style.backgroundImage = "url('')";
+}
